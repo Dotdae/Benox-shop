31f8e8b4296eb47b5fac73afb8558fd14b086787
Dotdae <chriiswho@gmail.com>
2022-05-16 15:03:48 -0600

    Esquema principal

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+    <?php require 'views/head.php' ?>
+</head>
+<body>
+    <?php require 'views/nav.php' ?>
+    <div class="container">
+        <div class="slider-container">
+            <div class="slideshow-container">
+                <div class="mySlides fade">
+                    <img src="https://areajugones.sport.es/wp-content/uploads/2020/01/one-piece-967-roger-riendo.jpg" style="width: 100%;">
+                    <div class="text">Caption</div>
+                </div>
+                <div class="mySlides fade">
+                    <img src="https://tierragamer.com/wp-content/uploads/2021/06/One-Piece-Gol-D-Roger-Rayleigh-Oden.jpg" style="width: 100%;">
+                    <div class="text">Caption</div>
+                </div>
+                <div class="mySlides fade">
+                    <img src="https://areajugones.sport.es/wp-content/uploads/2020/01/one-piece-967-roger-riendo.jpg" style="width: 100%;">
+                    <div class="text">Caption</div>
+                </div>
+                <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+                <a class="next" onclick="plusSlides(1)">&#10095;</a>
+            </div>
+            <div style="text-align: center;">
+                <span class="dot" onclick="currentSlide(1)"></span>
+                <span class="dot" onclick="currentSlide(2)"></span>
+                <span class="dot" onclick="currentSlide(3)"></span>
+            </div>
+        </div>
+        <div class="product-container">
+            <?php for($i = 0; $i <= 7; $i++) {?>
+            <div class="product-box">
+                    <div class="product-img">
+                        <a class="add-cart">
+                            <i class="fas fa-shopping-cart"></i>
+                        </a>
+                        <img src="https://i.pinimg.com/originals/a9/c5/3f/a9c53fa1ae331e1ae97c87cfc1e33c93.jpg" >
+                    </div>
+                    <div class="product-details">
+                    <a class="p-name"> Arqueóloga</a>
+                    <div class="bills-details">
+                        <img src="./assets/img/doge.png">
+                        <span class="p-price">$2000</span>
+                    </div>
+                </div>
+            </div>
+            <?php } ?>
+        </div>
+    </div>
+    <script src="./assets/js/slider.js"></script>
+</body>
+</html>
